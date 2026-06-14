5fc773934ac72ecbc4cf9f88b5b2af0d856dfd40
martinZonis <49437822@est.ort.edu.ar>
2026-06-14 02:15:33 -0300

    motores

--- motoresMoverse/src/main.cpp
+++ motoresMoverse/src/main.cpp
@@ -1,18 +1,47 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define AIN1 25
+#define AIN2 33
+#define BIN1 27
+#define BIN2 26
+#define PWMA 32
+#define PWMB 14
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+  pinMode(PWMA, OUTPUT);
+  pinMode(PWMB, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
+  digitalWrite(AIN1, HIGH);
+  digitalWrite(AIN2, LOW);
+  digitalWrite(BIN1, HIGH);
+  digitalWrite(BIN2, LOW);
+  digitalWrite(PWMA, HIGH);
+  digitalWrite(PWMB, HIGH);
+  delay(2000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, HIGH);
+  digitalWrite(BIN1, LOW);
+  digitalWrite(BIN2, HIGH);
+  analogWrite(PWMA, 255);
+  analogWrite(PWMB, 255);
+  digitalWrite(PWMA, HIGH);
+  digitalWrite(PWMB, HIGH);
+  delay(2000);
+
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, LOW);
+  digitalWrite(BIN1, LOW);
+  digitalWrite(BIN2, LOW);
+  digitalWrite(PWMA, HIGH);
+  digitalWrite(PWMB, HIGH);
+  delay(2000);
 }
